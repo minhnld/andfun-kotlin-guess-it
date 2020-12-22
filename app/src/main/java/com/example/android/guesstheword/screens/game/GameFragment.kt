21b50f13da7700f512d1ef5652a53af2c8ac06b3
Minh Nguyen - 6679 <minh.dang.nguyen@logigear.com>
2020-12-22 11:15:41 +0700

    Complete step 1

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -22,6 +22,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -41,7 +43,7 @@ class GameFragment : Fragment() {
     private lateinit var wordList: MutableList<String>
 
     private lateinit var binding: GameFragmentBinding
-
+    private  lateinit var viewModel : GameViewModel
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
@@ -55,6 +57,7 @@ class GameFragment : Fragment() {
 
         // TODO (04) Create and initialize a GameViewModel, using ViewModelProvider; Add a log
         // statement
+        viewModel=ViewModelProvider(this).get(GameViewModel::class.java)
 
         resetList()
         nextWord()
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -16,5 +16,18 @@
 
 package com.example.android.guesstheword.screens.game
 
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
 // TODO (02) Create the GameViewModel class, extending ViewModel
+class GameViewModel:ViewModel () {
+    init{
+        Log.i("Game View Model", "GameView Created")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel","GameViewModel Destroy")
+    }
+}
 // TODO (03) Add init and override onCleared; Add log statements to both
